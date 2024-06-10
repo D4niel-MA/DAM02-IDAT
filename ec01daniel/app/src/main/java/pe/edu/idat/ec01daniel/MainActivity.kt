055eb9a1b68bef97f8a90c3608517b745b6cd253
Daniel <daniel.macha@outlook.es>
2024-06-09 20:10:53 -0500

    PRIMER COMMIT EC01-IDAT

---
+++ ec01daniel/app/src/main/java/pe/edu/idat/ec01daniel/MainActivity.kt
@@ -0,0 +1,40 @@
+package pe.edu.idat.ec01daniel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pe.edu.idat.ec01daniel.ui.theme.Ec01danielTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ec01danielTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ec01danielTheme {
+    }
+}
